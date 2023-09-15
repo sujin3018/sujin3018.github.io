1731c022898df864b2d7d97d04b3a9c29d426ed9
김수진 <130522616+sujin3018@users.noreply.github.com>
2023-09-15 16:53:21 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
@@ -1,7 +1,6 @@
 function solution(n, k) {
     
-    if (n % 10 >= 0) {
-     answer = 12000 * n + 2000 *(k- parseInt(n / 10))
-    }
-    return answer;
+   return  12000 * n + 2000 *(k- parseInt(n / 10))
+    
+   
 }
